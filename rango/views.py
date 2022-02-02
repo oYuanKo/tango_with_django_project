9f1511b251cd973bc600ea177d64f38f88691836
Yuan Kuang <2672762k@student.gla.ac.uk>
2022-02-02 14:31:15 +0000

    C

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    context_dict = {'boldnessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
 def about(request):
-    context_dict = {'boldnessage': 'This tutorial has been put together by Yuan Kuang.'}
+    context_dict = {'boldmessage': 'This tutorial has been put together by Yuan Kuang.'}
     return render(request, 'rango/about.html', context=context_dict)
 
