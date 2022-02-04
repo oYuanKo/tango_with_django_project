a120c73de2a13922ea5bc860dfba97e47766c4bd
Yuan Kuang <2672762k@student.gla.ac.uk>
2022-02-04 00:49:19 +0000

    Comment

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
 
