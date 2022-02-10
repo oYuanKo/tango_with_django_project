d7dbb69fcea9a537439d65e1623685e5df0d6187
Yuan Kuang <2672762k@student.gla.ac.uk>
2022-02-10 15:56:19 +0000

    Chap10withErrors

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
 ]
 
 ROOT_URLCONF = 'tango_with_django_project.urls'
