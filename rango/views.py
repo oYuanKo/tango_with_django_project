361574c6f384643985924e897f80054f9ca4c76b
Yuan Kuang <2672762kstudent.gla.ac.uk>
2024-01-30 15:15:44 +0000

    past Chapter 3 Tests

--- rango/views.py
+++ rango/views.py
@@ -3,13 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    link_to_about = '<a href="/rango/about/">here</a>'
-    response = f'Rango says hey there partner! Here is the about page {link_to_about}.'
-    return HttpResponse(response)
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
 
 
 def about(request):
-    link_to_index = '<a href="/rango/">Index</a>'
-    response = f'Rango says here is the about page! Go back to the {link_to_index}.'
-    return HttpResponse(response)
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
 
