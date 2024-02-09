7f8419b3fa27bd0afc3bf70bf0b5b4d419d7c760
Yuan Kuang <2672762kstudent.gla.ac.uk>
2024-02-09 12:35:49 +0000

    finish Chapter 9 with tests

--- rango/forms.py
+++ rango/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from rango.models import Page, Category
-
+from rango.models import Page, Category, UserProfile
+from django.contrib.auth.models import User
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="Please enter the category name.")
@@ -25,11 +25,31 @@ class PageForm(forms.ModelForm):
 
         exclude = ("category",)
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get("url")
+        if url and not url.startswith("http://"):
+            url = f"http://{url}"
+            cleaned_data["url"] = url
+        return cleaned_data
 
-def clean(self):
-    cleaned_data = self.cleaned_data
-    url = cleaned_data.get("url")
-    if url and not url.startswith("http://"):
-        url = f"http://{url}"
-        cleaned_data["url"] = url
-    return cleaned_data
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "password",
+        )
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = (
+            "website",
+            "picture",
+        )
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -21,6 +21,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, "media")
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = "/media/"
+LOGIN_URL = "rango:login"
 
 
 # Quick-start development settings - unsuitable for production
@@ -36,6 +37,14 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+
+PASSWORD_HASHERS = (
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.BCryptPasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+)
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -102,6 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 6,
+        },
     },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
